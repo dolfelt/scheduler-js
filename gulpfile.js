98eb4d7c95b6c68198c4e2a01627805c7e8ee3e5
Daniel Olfelt <dolfelt@gmail.com>
2015-08-26 22:02:34 -0500

    Add sourcemaps for sass files and cleanup whitespace.

--- gulpfile.js
+++ gulpfile.js
@@ -15,20 +15,20 @@ var connect = require("gulp-connect");
 
 // Configuration
 var output = {
-  'js': './dist/assets/js',
-  'css': './dist/assets/css'
+    'js': './dist/assets/js',
+    'css': './dist/assets/css'
 };
 var vendorFiles = [
-  'node_modules/mithril/mithril.js',
-  'node_modules/underscore/underscore.js'
+    'node_modules/mithril/mithril.js',
+    'node_modules/underscore/underscore.js'
 ];
 
 
 // add custom browserify options here
 var customOpts = {
-  entries: ['./src/app.js'],
-  paths: ['./node_modules','./src'],
-  debug: true
+    entries: ['./src/app.js'],
+    paths: ['./node_modules','./src'],
+    debug: true
 };
 var opts = assign({}, watchify.args, customOpts);
 var b = watchify(browserify(opts));
@@ -40,7 +40,7 @@ gulp.task("bundle", bundle);
 b.on('log', gutil.log); // output build logs to terminal
 
 function bundle() {
-  return b.bundle()
+    return b.bundle()
     // log errors if they happen
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('app.js'))
@@ -48,44 +48,46 @@ function bundle() {
     .pipe(buffer())
     // optional, remove if you dont want sourcemaps
     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-       // Add transformation tasks to the pipeline here.
+    // Add transformation tasks to the pipeline here.
     .pipe(sourcemaps.write('./')) // writes .map file
     .pipe(gulp.dest(output.js));
 }
 
 gulp.task("vendor", function() {
-  return gulp.src(vendorFiles)
+    return gulp.src(vendorFiles)
     .pipe(sourcemaps.init())
-      .pipe(concat('vendor.js'))
+    .pipe(concat('vendor.js'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(output.js));
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/[^_]*.scss')
+    return gulp.src('./sass/[^_]*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(output.css));
 });
 
 gulp.task("build", ["vendor", "bundle", "sass"], function() {
-  return gulp.src("./src/**/*.html")
+    return gulp.src("./src/**/*.html")
     .pipe(gulp.dest('./dist'));
 });
 
 
 
 gulp.task("serve", ["build", "watch"], function() {
-  return connect.server({
-    root: "./dist",
-    livereload: true,
-    fallback: "./dist/index.html"
-  });
+    return connect.server({
+        root: "./dist",
+        livereload: true,
+        fallback: "./dist/index.html"
+    });
 });
 
 gulp.task("watch", function () {
-  b.on('update', bundle); // on any dep update, runs the bundler
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  return bundle();
+    b.on('update', bundle); // on any dep update, runs the bundler
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    return bundle();
 });
 
 gulp.task("default", ["build", "watch"]);
